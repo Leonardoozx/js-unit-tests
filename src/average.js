ded43332fa2d47aaa32785fc4ee40eb0a3826811
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-04-06 15:36:15 -0300

    complete the average requirement

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let numbers = 0;
+  let result = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    numbers += array[i];
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+  }
+  result = numbers / array.length;
+  return Math.round(result);
+};
 
 module.exports = average;
